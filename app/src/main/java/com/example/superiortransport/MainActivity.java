f6228ce072c6bc155010bf8833986519c862f313
Zaigham Ali <sultanzaigham@gmail.com>
2019-11-29 00:11:30 +0500

    2nd changes

--- app/src/main/java/com/example/superiortransport/MainActivity.java
+++ app/src/main/java/com/example/superiortransport/MainActivity.java
@@ -1,11 +1,11 @@
 package com.example.superiortransport;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -14,7 +14,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
         mDriver = (Button) findViewById(R.id.driver);
         mCustomer = (Button) findViewById(R.id.customer);
         mDriver.setOnClickListener(new View.OnClickListener() {
